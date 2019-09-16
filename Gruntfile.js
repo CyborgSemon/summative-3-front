00fcac4a2c0cf1f33140c8ed5e4176c44569c664
Simon <watsonsemon@gmail.com>
2019-09-17 11:26:02 +1200

    added grunt modules

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,7 +1,26 @@
 module.exports = function(grunt) {
-
 	grunt.initConfig({
-
+		sass: {
+			dist: {
+				files: {
+					'css/main.css': 'scss/main.scss'
+				}
+			}
+		},
+		cssmin: {
+			target: {
+				files: [{
+					expand: true,
+					cwd: 'css/',
+					src: ['*.css', '!*.min.css'],
+					dest: 'css/',
+					ext: '.min.css'
+				}]
+			}
+		},
 	});
+
+	grunt.loadNpmTasks('grunt-contrib-sass');
+	grunt.loadNpmTasks('grunt-contrib-cssmin');
 
 }

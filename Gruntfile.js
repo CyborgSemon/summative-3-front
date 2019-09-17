13081831f6f656019d9749c28c658c14357431fb
BraydenGray416 <brayden.gray98@gmail.com>
2019-09-17 14:53:15 +1200

    Created jshint and uglify both allowing es6 and created a watch

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,7 +1,33 @@
 module.exports = function(grunt) {
 
 	grunt.initConfig({
-
+		uglify: {
+			my_target: {
+				files: {
+					'js/main.min.js': ['js/main.js']
+				},
+				options: {
+					esversion: 6
+				}
+			}
+		},
+		jshint: {
+			files: ['js/*.js', '!js/*.min.js'],
+			options: {
+				esversion: 6
+			}
+		},
+		watch: {
+			js: {
+				files: ['js/*.js', '!js/*.min.js'],
+				tasks: ['jshint', 'uglify']
+			}
+		}
 	});
 
+	grunt.loadNpmTasks('grunt-contrib-watch');
+	grunt.loadNpmTasks('grunt-contrib-jshint');
+	grunt.loadNpmTasks('grunt-contrib-uglify-es');
+
+	grunt.registerTask('default', ['watch']);
 }

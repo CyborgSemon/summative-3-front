166c5a09e7760cd7c8b5e9dcb66764974efdfc56
Simon <watsonsemon@gmail.com>
2019-09-18 11:11:44 +1200

    added login form

--- js/main.js
+++ js/main.js
@@ -1,2 +1,47 @@
-console.log('JS has loaded');
-console.log(`yeet`);
+let url;
+let mapsKey;
+
+$.ajax({
+	url: `config.json`,
+	type: `GET`,
+	dataType: 'json',
+	error: (err)=> {
+		console.log(`There was an issue getting the config file`);
+		console.log(err);
+	},
+	success: (result)=> {
+		url = `${result.SERVER_URL}:${result.SERVER_PORT}`;
+		mapsKey = result.GOOGLE_MAPS_KEY;
+	}
+});
+
+$(`#loginForm`).submit(()=> {
+	event.preventDefault();
+	let loginUsername = $(`#loginUsername`).val();
+	let loginPassword = $(`#loginPassword`).val();
+
+	if (loginUsername.length !== 0 && loginPassword.length !== 0) {
+		$.ajax({
+			url: `${url}/login`,
+			type: `POST`,
+			data: {
+				username: loginUsername,
+				password: loginPassword
+			},
+			error: (err)=> {
+				console.log(`Error logging in`);
+				console.log(err);
+			},
+			success: (result)=> {
+				sessionStorage.userId = result._id;
+				sessionStorage.username = result.username;
+				sessionStorage.name = result.name;
+				sessionStorage.email = result.email;
+				sessionStorage.address = result.address;
+			}
+		});
+	} else {
+		console.log('You have not filled in all the login inputs');
+	}
+});
+

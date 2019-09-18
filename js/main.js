c390a5280499cfdeb94c554b127eb43028ebf2f0
BraydenGray416 <brayden.gray98@gmail.com>
2019-09-18 16:28:40 +1200

    Created front end for update and delete listings

--- js/main.js
+++ js/main.js
@@ -1,2 +1,53 @@
 console.log('JS has loaded');
 console.log(`yeet`);
+
+$(`#listingList`).on(`click`, `.editBtn` ()=> {
+    event.preventDefault();
+    if (!sessionStorage.userId) {
+        alert(`401, permission denied`);
+        return;
+    }
+    // const id = (idk what the target was)
+    $.ajax({
+        url: `${url}/listing/${id}`,
+        type: `post`,
+        data: {
+            userId: sessionStorage.userId
+        },
+        dataType: `json`,
+        success:function(product){
+            console.log(product);
+            $(`#listingTitle`).val(product.title);
+            $(`#listingDescription`).val(product.description);
+            $('#listingPrice').val(product.price);
+            $(`#addProductButton`).text(`Edit Listing`).addClass(`btn-warning`);
+            $(`#heading`).text(`Edit Product`);
+            editing = true;
+        },
+        error:function(err){
+            console.log(err);
+            console.log(`something went wrong with getting the single product`);
+        }
+    })
+});
+
+$(`#productList`).on(`click`, `.removeBtn`, function(){
+    event.preventDefault();
+    if (!sessionStorage.userId) {
+        alert(`401, permission denied`);
+        return;
+    }
+    // const id = $(this).parent().parent().data(`id`);
+    // const li = $(this).parent().parent();
+    $.ajax({
+        url: `${url}/listing/${id}`,
+        type: `DELETE`,
+        success:function(result){
+            li.remove();
+        },
+        error:function(err) {
+            console.log(err);
+            console.log(`something went wrong deleting the product`);
+        }
+    })
+});
